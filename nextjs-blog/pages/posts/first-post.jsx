e576dc52cd06b50f88a4615463b6b047e428c44c
Andrew TOwnsley <atownsley86@gmail.com>
2021-11-29 16:35:38 -0700

    Initial file setup for next docs tutorial.

---
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+const FirstPost = () =>  {
+    return <div>
+
+        <Head>
+            <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">
+                <a>back to home</a>
+            </Link>
+        </h2>
+        <Image
+            src="/images/profile-img.jpg"
+            height={100}
+            width={75}
+            alt="andrew"
+        />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat maxime qui quo ullam molestiae   fuga in beatae, vero eligendi sapiente fugit quod eveniet adipisci reiciendis mollitia cum magnam     inventore? Rem consectetur cupiditate ipsam modi iste voluptates labore vel voluptas enim quod  dolorem reiciendis, ex ipsum aut pariatur hic totam impedit.</p>
+    </div>
+
+}
+
+export default FirstPost
